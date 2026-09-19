d299f44fa4dd4adb6c83f9ab7c1cf2292e4ee842
Shivam Bind <shivambind594@gmail.com>
2026-09-19 20:09:24 +0530

    Revert to stable navbar and app layout

--- src/App.jsx
+++ src/App.jsx
@@ -33,26 +33,26 @@ function App() {
       <FilterBar activeCategory={activeCategory} onCategoryChange={setActiveCategory} />
       <Carousel />
 
- <main className="page-content">
-  <PromoCards />
+      <main className="page-content">
+        <PromoCards />
 
-  <ProductSlider
-    title="Up to 40% off | Must-Have Kitchen Appliances"
-    products={kitchenProducts}
-    onAddToCart={addToCart}
-  />
-  <ProductSlider
-    title="Up to 70% off | Bestselling SmartWatches"
-    products={watchProducts}
-    onAddToCart={addToCart}
-  />
+        <ProductSlider
+          title="Up to 40% off | Must-Have Kitchen Appliances"
+          products={kitchenProducts}
+          onAddToCart={addToCart}
+        />
+        <ProductSlider
+          title="Up to 70% off | Bestselling SmartWatches"
+          products={watchProducts}
+          onAddToCart={addToCart}
+        />
 
-  {noResults && (
-    <p style={{ textAlign: 'center', fontWeight: 600, marginTop: '20px' }}>
-      No results found
-    </p>
-  )}
-</main>
+        {noResults && (
+          <p style={{ textAlign: 'center', fontWeight: 600, marginTop: '20px' }}>
+            No results found
+          </p>
+        )}
+      </main>
 
       <Footer />
     </>
